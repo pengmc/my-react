df3542eca5a3d44f790d338969bac0b776293c8c
彭民材 <717372061@qq.com>
2022-08-17 10:43:20 +0800

    cs

--- src/api/api.js
+++ src/api/api.js
@@ -2,8 +2,11 @@ import axios from "axios";
 import { Toast } from "antd-mobile";
 import { history } from "../history";
 
+// const baseurl = "/proxy";
+const baseurl = "https://node-music-cyan.vercel.app";
+
 const api = axios.create({
-  baseURL: "/proxy",
+  baseURL: baseurl,
   timeout: 10000,
 });
 
